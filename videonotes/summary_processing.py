f848f5b3d029a980f179bc09becd7c2182655471
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2024-03-14 05:43:22 +0100

    Refactor

--- videonotes/summary_processing.py
+++ videonotes/summary_processing.py
@@ -1,22 +1,5 @@
 import os
-from openai import OpenAI
-
-client = OpenAI()
-
-def chat_with_openai(prompt):
-    message = {
-        'role': 'user',
-        'content': prompt
-    }
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[message],
-        max_tokens=1500
-    )
-
-    chatbot_response = response.choices[0].message.content
-    return chatbot_response.strip()
+from .llm.openai.openai_chat import chat_with_openai
 
 def summarize_transcription(transcription_filename):
     print(f"Summarizing {transcription_filename}")
